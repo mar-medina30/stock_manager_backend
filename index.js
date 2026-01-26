96f5d0abc76fcf1dba474b437749b7af4db6320d
Martina <martinamedinaprogramacion@gmail.com>
2026-01-26 16:52:56 -0300

    express.Router en ingreso

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 
 import productoRoute from "./routes/productos.js"
 import egresoRoute from "./routes/egreso.js"
+import ingresoRoute from "./routes/ingresos.js"
 
 const conexion = await iniciardb()
 const app = express()
@@ -10,6 +11,7 @@ const port = 3000
 app.use(express.json())
 app.use("/producto", productoRoute)
 app.use("/egreso", egresoRoute)
+app.use("/ingreso", ingresoRoute)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
